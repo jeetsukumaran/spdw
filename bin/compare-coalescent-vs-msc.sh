614733147dd27b45fb67df572121cc8118b26aaa
Jeet Sukumaran <jeetsukumaran@gmail.com>
2019-06-12 01:41:28 -0700

    Fix package

--- bin/compare-coalescent-vs-msc.sh
+++ bin/compare-coalescent-vs-msc.sh
@@ -2,14 +2,14 @@
 
 set -e -o pipefail
 
-python bin/spdw-sim-coaltrees.py \
+spdw-sim-coaltrees.py \
     -k 100 \
     --num-reps 100 \
     coal1
-python bin/spdw-sim-bdstruct-coaltrees.py \
+spdw-sim-bdstruct-coaltrees.py \
     -p50 \
     -k2 \
     --num-pop-tree-reps 1 \
     --num-reps-per-pop-tree 100 \
     bd1
-python bin/spdw-plotcoaltimes.py *.trees
+spdw-plotcoaltimes.py *.trees
--- src/spdw/__init__.py
+++ src/spdw/__init__.py
@@ -1,4 +1,22 @@
 #! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 __version__ = "1.0.0"
 __project__ = "spdw"
+
+
+def render_output(args, name):
+    if args.output_prefix:
+        # ax.get_figure().savefig(
+        # ax.savefig(
+        # ax.figure.savefig(
+        plt.savefig(
+                compose_output_path(args.output_prefix, name, args.output_format),
+                bbox_inches="tight")
+    if args.show_plot_on_screen is not False:
+        plt.show()
+
